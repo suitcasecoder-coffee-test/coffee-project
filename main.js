e543e0e1455acb271a66306e77eb9585b3cad074
laura <laura@codeup.com>
2023-04-14 15:15:36 -0500

    existing code walkthrough

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 "use strict"
 
+
+// display the individual coffees
+// displays 1 coffee
+
 function renderCoffee(coffee) {
+
+    // currently creating a table --> divs/p's
     var html = '<tr class="coffee">';
     html += '<td>' + coffee.id + '</td>';
     html += '<td>' + coffee.name + '</td>';
@@ -9,26 +15,61 @@ function renderCoffee(coffee) {
 
     return html;
 }
+// <tr class="coffee">
+//     <td>1</td>
+//     <td>American</td>
+//     <td>Light</td>
+// </tr>
+// <tr class="coffee">
+//     <td>1</td>
+//     <td>American</td>
+//     <td>Light</td>
+// </tr>
+// <tr class="coffee">
+//     <td>1</td>
+//     <td>American</td>
+//     <td>Light</td>
+// </tr>
 
+
+// accepts an array of coffees and loops through them and then passes each individual coffee into `renderCoffee`
 function renderCoffees(coffees) {
     var html = '';
     for(var i = coffees.length - 1; i >= 0; i--) {
+        console.log(coffees[i]);
         html += renderCoffee(coffees[i]);
     }
     return html;
 }
 
+
+//
 function updateCoffees(e) {
     e.preventDefault(); // don't submit the form, we just want to update the data
     var selectedRoast = roastSelection.value;
+    console.log(selectedRoast);
     var filteredCoffees = [];
+
+    // loop through each of the coffee objects and we are adding the coffee's who's roast match the selected roast and adding it to the filteredCoffees array
     coffees.forEach(function(coffee) {
         if (coffee.roast === selectedRoast) {
             filteredCoffees.push(coffee);
         }
     });
+
+    //
+    console.log(tbody);
     tbody.innerHTML = renderCoffees(filteredCoffees);
 }
+
+
+
+// ----- HANDLE INPUT FIELD -----
+
+
+// ----- HANDLE ADD A COFFEE -----
+
+
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
